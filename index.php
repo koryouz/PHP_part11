71824adbc840e0ea48804d5e43e91193a1ec3227
DELHAY <delhayantoine@gmail.com>
2019-04-04 13:52:18 +0200

    Ajout de la partie 11

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>PHP P11 HOME</title>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+        <h1>HOME</h1>
+      <p><a href="http://p11/tp1">Lien vers le TP 1</a></p>  
+    </div>
+  </div>
+</body>
+</html>
